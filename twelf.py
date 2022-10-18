704add38c7831712c45326eaa4482e6e1062a654
Mroik <mroik@delayed.space>
2022-10-18 16:17:23 +0200

    Add type definition & function definition parsing
    Type definition does not support polymorphism and function definition
    does not support infixing.

--- twelf.py
+++ twelf.py
@@ -68,8 +68,44 @@ class Lexer:
 
 
 class Parser:
-    def __init__(self):
-        self._grammar = None
+    def __init__(self, lexer: Lexer):
+        self._lexer = lexer
+        self._types = []
+        self._functions = {}
+        self._rules = {}
+
+    # Does not support polymorphism
+    def _parse_type_definition(self):
+        if self._lexer.peek()[0] in self._types:
+            raise Exception("TODO")
+        self._types.append(self._lexer.consume(4)[0])
+
+    # Does not support infixing
+    def _parse_function_definiton(self):
+        parameters = []
+        name = self._lexer.consume()[0]  # TODO check if already exist
+        if self._lexer.consume()[0] != ":":
+            raise Exception("TODO")
+        while True:
+            parameters.append(self._lexer.consume()[0])
+            ender = self._lexer.consume()[0]
+            if ender == ".":
+                break
+            if ender != "->":
+                raise Exception("TODO")
+        self._functions[name] = parameters
+
+    def _parse_rule_definition(self):
+        steps = []
+        name = self._lexer.consume(2)[0]
+        i = 1
+        while True:
+            if self._lexer.peek(i)[i - 1] == "<-" or ".":
+                # TODO check if function matches any existing ones
+                pass
+
+    def _parse_expression(self):
+        pass
 
     def parse(self):
         pass
